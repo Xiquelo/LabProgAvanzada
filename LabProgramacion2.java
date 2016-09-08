ec0adca8db6847bd6d26a5f156e3dc1f076146b3
Xiquelo <xiquelo.rosalia@gmail.com>
2016-09-08 08:50:46 -0500

    Add files via upload

---
+++ LabProgramacion2.java
@@ -0,0 +1,9 @@
+public class LabProgramacion2
+{
+	public static void main(String[] args)
+	{
+		Metodos objeto = new Metodos();
+		objeto.Carrito();
+		objeto.Modelo();
+	}
+}
---
+++ Metodos.java
@@ -0,0 +1,13 @@
+
+public class Metodos
+{
+	public void Carrito()
+	{
+		System.out.println("Invocando metodo de otra clase");
+	}
+
+	public void Modelo()
+	{
+		System.out.println("Pointer");
+	}
+}
